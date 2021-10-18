316e4b3fa708398eaa7d3146f16d8573859cf782
Tiago Pierre <tiago_pierre@outlook.com>
2021-10-18 10:55:15 -0300

    changing container padding on smaller devices

--- scripts/backToTop.js
+++ scripts/backToTop.js
@@ -3,7 +3,7 @@ const backToTopButton = document.querySelector('.back-to-top')
 window.addEventListener('scroll', function () {
   if (window.scrollY >= 560) {
     backToTopButton.style.visibility = 'visible'
-    backToTopButton.style.bottom = '3rem'
+    backToTopButton.style.bottom = '1rem'
   } else {
     backToTopButton.style.visibility = 'hidden'
     backToTopButton.style.bottom = '-5rem'
